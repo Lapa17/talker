3927292e97dd338e1c3e17c6d2dbed0e8a852b38
Lapa17 <laparevichpavel@gmail.com>
2022-05-05 16:02:37 +0300

    Initial commit

---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+import {MainWindow} from './MainWindow'
+import {CallModal} from './CallModal'
+import {CallWindow} from './CallWindow'
+
+
+export {
+    CallModal,
+    CallWindow,
+    MainWindow,
+}
